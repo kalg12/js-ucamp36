66226362f0608701fc89bda64445c9c9e122cb5a
Kevin Luciano <78965771+kalg12@users.noreply.github.com>
2025-03-06 20:38:50 -0600

    Bootstrap + JS

--- js/main.js
+++ js/main.js
@@ -8,3 +8,20 @@ Operaciones matemáticas
 Operadores
 Condicionales
 */
+
+const nombre = "Kevin";
+const miCp = 39640;
+const apellidoPaterno = "Luciano";
+
+console.log(nombre);
+console.log(apellidoPaterno);
+console.log(miCp);
+console.log(5 + 5);
+console.log("5+5");
+console.log(5 * 5); // 25
+console.log(nombre + " " + apellidoPaterno);
+console.log(5 - 5); //0
+console.log(10 / 2); //5
+
+/* CAMBIAR LOS CONSOLE.LOG POR ALERTS */
+alert(5 + 5);
