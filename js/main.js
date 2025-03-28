b3d39eaf3966054b5fe6f6c54c2c232460ce4bd3
Kevin Luciano <78965771+kalg12@users.noreply.github.com>
2025-03-27 19:37:30 -0600

    Ultimos cambios

--- js/main.js
+++ js/main.js
@@ -1,27 +1,66 @@
-/* Para imprimir en pantalla usamos console.log() */
-console.log("Hola ucamp!");
-
-/* En la siguiente semana veremos:
-Uso de variables
-Concatenación
-Operaciones matemáticas
-Operadores
-Condicionales
+/*
+Objetos en Javascript
 */
 
-const nombre = "Kevin";
-const miCp = 39640;
-const apellidoPaterno = "Luciano";
+let estudiante = {
+  nombre: "Noé",
+  edad: 15,
+  isActive: true,
+};
 
-console.log(nombre);
-console.log(apellidoPaterno);
-console.log(miCp);
-console.log(5 + 5);
-console.log("5+5");
-console.log(5 * 5); // 25
-console.log(nombre + " " + apellidoPaterno);
-console.log(5 - 5); //0
-console.log(10 / 2); //5
+console.log(typeof estudiante);
+console.log(estudiante);
 
-/* CAMBIAR LOS CONSOLE.LOG POR ALERTS */
-alert(5 + 5);
+if (estudiante.edad >= 18) {
+  console.log("Eres mayor de edad");
+} else {
+  console.log("Eres menor de edad");
+}
+
+/* ARREGLOS */
+
+let estudiantes = [
+  "Noé",
+  "Alan",
+  "Abel",
+  "Veronica",
+  "Luis",
+  "Itzel",
+  "Leonel",
+  "Sergio",
+];
+console.log(typeof estudiantes);
+console.log(estudiantes);
+console.log(estudiantes[6]);
+
+const zoologico = ["🐯", "🦁", "🐻", "🐵", "🐘", "🦒", "🦓", "🦔"];
+console.log(zoologico);
+console.log("El zoológico tiene " + zoologico.length + " animales");
+/* Kevin ha logrado tener más ingresos $$$$  y compra un delfin para su zoologico */
+zoologico.push("🐬");
+console.log(zoologico);
+console.log("El zoológico tiene " + zoologico.length + " animales");
+/* Kevin ha logrado tener más ingresos $$$$  y 3 animales más */
+zoologico.push("🐍", "🦑", "🦈");
+console.log(zoologico);
+console.log("El zoológico tiene " + zoologico.length + " animales");
+/* Kevin pierde al último animal que compró */
+zoologico.pop();
+console.log(zoologico);
+console.log("El zoológico tiene " + zoologico.length + " animales");
+
+/* Vamos a usar el métofo por each para recorrer el arreglo llamado zoolico */
+zoologico.forEach((emojiDeCadaAnimal) => {
+  console.log(emojiDeCadaAnimal);
+});
+
+console.log("------------------------------------------------");
+console.log(zoologico[0]);
+console.log(zoologico[1]);
+console.log(zoologico[2]);
+console.log(zoologico[3]);
+
+/* Quiero eliminar del zooligo al elemento pocisionado en la 3 */
+zoologico.splice(3, 1);
+console.log(zoologico);
+console.log("El zoológico tiene " + zoologico.length + " animales");
